a28ef44648aaf17975e7b487b50699210e56eb16
rafiadpr <work.rafiadrianp@gmail.com>
2026-03-03 11:45:47 +0700

    [update] : finished js3 prak2

---
+++ database/migrations/2026_03_03_044402_create_m_category_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('m_category', function (Blueprint $table) {
+            $table->id('category_id');
+            $table->string('category_kode', 10)->unique();
+            $table->string('category_nama', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('m_category');
+    }
+};
